6357e097b8f6cd797203172fd967fe4eb1274a3e
npganga <gangangpalayam@gmail.com>
2017-07-04 09:55:36 +0530

    Add files via upload

---
+++ Hunter32.java
@@ -0,0 +1,22 @@
+package guviworkspace;
+
+import java.util.Scanner;
+
+public class Hunter32 {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int size=in.nextInt();
+	//int[] array=new int[size];
+	/*for(int i=0;i<size;i++){
+		array[i]=in.nextInt();
+	}*/
+	int count=0;
+	while(size>=1){
+		size=size/2;
+		count++;
+	}
+	int d=(int)Math.pow(2,count-1);
+	System.out.println(d);
+	in.close();
+}
+}
